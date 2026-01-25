a593d40517545e698c574c060b5ade38be002e00
Muhammad Alam <muhammad.alam10@dcmail.ca>
2026-01-24 19:11:50 -0500

    The Beginning of the end lets goooo

---
+++ marketplace/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Welcome, JackAssess...</h1>
+      <p>
+        Witness the birth of the Student Marketplace.  
+        Is this the greatest thing ever, Debatable,
+         or the beginning of chaos? absolutelyy 
+       
+      </p>
+      <p>
+        Like watching the Mona Lisa being painted…  
+        or the Roman Empire’s slow downfall.
+      </p>
+      <p className="mystery">Who knows But it will be one hell of a ride over the course of next 9 weeks Fuck It we Ball</p>
+    </div>
+  );
+}
+
+export default App;
